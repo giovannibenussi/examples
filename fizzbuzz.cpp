2ca3b1ca2f951b89bfe5688f1c23263bd3f6b1e1
Giovanni <giovanni.benussi@usach.cl>
2013-05-18 20:28:16 -0400

    Fizzbuzz C++

---
+++ fizzbuzz.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	for(int i=1;i<=100;i++){
+		if(i%3==0 && i%5==0)
+			cout << "Fizz";
+		else if(i%3==0)
+			cout << "Fizz";
+		else if(i%5==0)
+			cout << "Buzz";
+		else
+            cout << i;
+		if(i != 100)
+			cout << ", ";
+	}
+}
+
